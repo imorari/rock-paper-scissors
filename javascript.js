d627299139db5e3057bd1049546c92decfdd9957
imorari <iulian.morari1995@gmail.com>
2023-10-25 12:41:05 -0400

    working game needs a little more style

--- javascript.js
+++ javascript.js
@@ -1,64 +1,165 @@
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const restartButton = document.querySelector('#restartButton')
 
+let computerScore = 0;
+let playerScore = 0;
+
+const weaponButtons = document.querySelectorAll('.weapon');
+weaponButtons.forEach(function(button) {
+    console.log(button.value);
+});
+
+
+// Declare variables to store references to event handler functions
+let rockButtonEvent;
+let paperButtonEvent;
+let scissorsButtonEvent;
+
+// Function to get a computer choice
 function getComputerChoice() {
-    let compChoiceArray = ['rock', 'paper', 'scissors'];
-    let rand = compChoiceArray[Math.floor(Math.random() * compChoiceArray.length)];
-    return rand;
+  let compChoiceArray = ['rock', 'paper', 'scissors'];
+  let rand = compChoiceArray[Math.floor(Math.random() * compChoiceArray.length)];
+  return rand;
 }
-//const playerSelection = prompt("Choose your fighter: ");                                 we are going to prompt the user for input everytime the playround function is called inside the game function
-//const computerSelection = getComputerChoice();                                            
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection===playerSelection) {
-        return undefined                                                                    //"It's a tie, both players selected " + playerSelection + " !";
-    } else if (
-        (computerSelection === 'paper' && playerSelection === 'rock') ||
-        (computerSelection === 'scissors' && playerSelection === 'paper') || 
-        (computerSelection === "rock" && playerSelection === "scissors")
-    ) {
-        return true                                                                         //"You Lose! " + computerSelection + " beats " + playerSelection + " !";
-    } else {
-        return false                                                                        //"You Win! " + playerSelection + " beats " + computerSelection + " !";
+
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice();
+  if (computerSelection === playerSelection) {
+    console.log("It's a tie");
+    
+  } else if (
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper') ||
+    (computerSelection === 'rock' && playerSelection === 'scissors')
+  ) {
+    
+    console.log("You win");
+    return true
+   
+  } else {
+    console.log("You lost");
+    return false
+    
+  }
+}
+
+
+
+// HTML Changes
+
+
+
+
+
+
+
+
+
+
+function eventHandle() {
+    
+
+    let out = playRound(this.value, getComputerChoice());
+  
+    if ((computerScore != 5) && (playerScore != 5)) {
+        if (out === true) {
+            computerScore += 1;
+            compScore.textContent=computerScore;
+            console.log("Computer wins this round "+ playerScore + ' '+ computerScore );
+            const historyItem = document.createElement('li')
+            historyItem.classList.add('removable')
+            historyItem.setAttribute('style','color: white; background-color: red; font-size: x-large')
+            historyItem.textContent = "Computer wins this round " + " player score is " +  playerScore + " computer score is "+ computerScore;
+            historyList.appendChild(historyItem)
+ 
+          } else if (out === false) {
+            playerScore += 1;
+            plScore.textContent = playerScore;
+            const historyItem = document.createElement('li')
+            historyItem.classList.add('removable')
+            historyItem.setAttribute('style','color: white; background-color: green; font-size: x-large')
+            historyItem.textContent = "Player wins this round "+ " player score is " + playerScore + " computer score is "+ computerScore; 
+            historyList.appendChild(historyItem)
+            console.log("Player wins this round " + playerScore + ' '+ computerScore );
+          } else if (out === undefined) {
+            let text="It's a tie this round "+ playerScore + ' '+ computerScore ;
+            const historyItem = document.createElement('li')
+            historyItem.classList.add('removable')
+            historyItem.setAttribute('style','color:white; background-color: blue; font-size: x-large')
+            historyItem.textContent = "This round is a Draw"  
+            historyList.appendChild(historyItem)
+            
+
+          }
     }
-}
-  //console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase()));
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    while (playerScore < 5 && computerScore < 5) {
-              const computerSelection = getComputerChoice();
-              let playerSelection;
-        while (true) { 
-            playerSelection = prompt("Choose your fighter: ").toLowerCase();           
-            if (playerSelection === "rock" || playerSelection === "scissors" || playerSelection === "paper") {   //condition for input validation
-                break;
-            } else {
-                alert("Your choices are \"paper\", \"rock\", or \"scissors\". Please try again.");
-            }
-        }
-        const result = playRound(playerSelection, computerSelection);                       //computer selection is already lower case so there is no need for toLowerCase function    
+    else{
+        console.log("The game should stop here");
+
+      // Remove the event handler from all buttons I do not want them to listen if I am not in a game
+      weaponButtons.forEach(button => {
+        button.removeEventListener('click', eventHandle);
+      });
+    }
+   
+    
+  }
+//add event listeners to all weapon buttons
+  weaponButtons.forEach(button => {
+    button.addEventListener('click', eventHandle);
+  });
+
+// remove the list items function
+
+function remove (){
+    const historyList= document.querySelector('ol')
+    const removableItem = historyList.querySelectorAll('.removable')
+    removableItem.forEach(item =>{
+       item.parentNode.removeChild(item);
+    });
         
-
-        if (result === undefined) {
-            // The round was skipped
-            console.log("It is a tie")                                              // added this note to know when there is a tie
-            console.log("Current computer score is: " + computerScore )
-            console.log("Current player score is: " + playerScore)
-        } else if (result === true) {
-            computerScore++;
-            console.log("Current computer score is: " + computerScore )
-            console.log("Current player score is: " + playerScore )
-        } else {
-            playerScore++;
-            console.log("Current computer score is: " + computerScore )
-            console.log("Current player score is: " + playerScore )
-        }
+        
     }
 
-    if (playerScore === 5) {
-        console.log("Player wins! with score: " + playerScore + " to " + computerScore);
-    } else {
-        console.log("Computer wins! with score "+ computerScore + " to " + playerScore);
-    }
 
- }
-// Start game
-game()
+
+restartButton.addEventListener('click', function() {
+
+    computerScore = 0;
+    playerScore = 0;
+    remove()
+
+    plScore.textContent = playerScore;
+    compScore.textContent = computerScore;
+  
+ 
+    weaponButtons.forEach(button => {
+      button.addEventListener('click', eventHandle);
+    });
+  
+    console.log("Game has been reset.");
+  });
+
+
+
+
+
+  const plScore = document.createElement('span');
+  const yourScoreDiv = document.querySelector('.playerScore');
+  yourScoreDiv.appendChild(plScore);
+
+  const compScore = document.createElement('span');
+  const compScoreDiv = document.querySelector('.computerScore');
+  compScoreDiv.appendChild(compScore);
+
+  
+  const gameHistory = document.querySelector('.fightZone')
+  
+  const historyList = document.querySelector('ol')
+
+
+
+  
+
+
